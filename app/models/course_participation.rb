5c8f44c5e8c887bbffd995937e6c83d500f19beb
zulkarnay <bedelbaev@gmail.com>
2018-03-19 14:04:40 +0600

    work with models

--- app/models/course_participation.rb
+++ app/models/course_participation.rb
@@ -1,4 +1,7 @@
 class CourseParticipation < ApplicationRecord
   belongs_to :course
   belongs_to :learner
+
+  validates :course_id, :learner_id, presence: true
+  validates :course_id, uniqueness: { scope: :learner_id }
 end
--- spec/models/course_participation_spec.rb
+++ spec/models/course_participation_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe CourseParticipation, type: :model do
     it { is_expected.to belong_to :course }
     it { is_expected.to belong_to :learner }
   end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of :course_id }
+    it { is_expected.to validate_presence_of :learner_id }
+
+    it { is_expected.to validate_uniqueness_of(:course_id).scoped_to(:learner_id) }
+  end
 end
